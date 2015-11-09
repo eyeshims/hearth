9911cc4eab8535217f30173a7d162c7d9ddbfd4b
eyeshims <eyeshims@gmail.com>
2015-11-09 16:22:46 +1300

    Replace toggle with array of iamges

--- app/src/main/java/com/hearth/MainActivity.java
+++ app/src/main/java/com/hearth/MainActivity.java
@@ -3,22 +3,31 @@ package com.hearth;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     GestureDetectorCompat mDetector;
-    boolean toggle = false;
+    Integer[] images;
+    Integer currentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
+        images = new Integer[] {
+            R.drawable.modern_apartment,
+            R.drawable.modern_apartment_2
+        };
+        currentImage = 0;
+        displayNextRentalProperty();
     }
 
     public void onYesOrNoButtonClick(View view) {
@@ -39,14 +48,12 @@ public class MainActivity extends AppCompatActivity {
     private void displayNextRentalProperty() {
         ImageView propertyPreview = (ImageView) findViewById(R.id.propertyPreview);
 
-        if (toggle) {
-            propertyPreview.setImageResource(R.drawable.modern_apartment);
-            toggle = false;
+        currentImage++;
+        if (currentImage == images.length) {
+            currentImage = 0;
         }
-        else {
-            propertyPreview.setImageResource(R.drawable.modern_apartment_2);
-            toggle = true;
-        }
+
+        propertyPreview.setImageResource(images[currentImage]);
     }
 
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
