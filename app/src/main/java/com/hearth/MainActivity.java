6ad5d02368774a2d94b4d6bf15713c10a7ee7d79
eyeshims <eyeshims@gmail.com>
2015-11-09 16:34:53 +1300

    Extract property display functionality into class

--- app/src/main/java/com/hearth/MainActivity.java
+++ app/src/main/java/com/hearth/MainActivity.java
@@ -7,14 +7,12 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.util.List;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    PropertyCollection properties;
     GestureDetectorCompat mDetector;
-    Integer[] images;
-    Integer currentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 
-        images = new Integer[] {
-            R.drawable.modern_apartment,
-            R.drawable.modern_apartment_2
-        };
-        currentImage = 0;
-        displayNextRentalProperty();
+        ImageView propertyPreview = (ImageView) findViewById(R.id.propertyPreview);
+        TextView progressText = (TextView) findViewById(R.id.progressText);
+        properties = new PropertyCollection(propertyPreview, progressText);
     }
 
     public void onYesOrNoButtonClick(View view) {
-        displayNextRentalProperty();
+        properties.showNextProperty();
     }
 
     @Override
@@ -39,21 +34,48 @@ public class MainActivity extends AppCompatActivity {
         Boolean wasFlingGesture = mDetector.onTouchEvent(event);
 
         if (wasFlingGesture) {
-            displayNextRentalProperty();
+            properties.showNextProperty();
         }
 
         return super.onTouchEvent(event);
     }
 
-    private void displayNextRentalProperty() {
-        ImageView propertyPreview = (ImageView) findViewById(R.id.propertyPreview);
+    class PropertyCollection {
+        ImageView propertyPreview;
+        TextView progressText;
 
-        currentImage++;
-        if (currentImage == images.length) {
-            currentImage = 0;
+        Integer[] images;
+        Integer imageIndex;
+
+        public PropertyCollection(ImageView propertyPreview, TextView progressText) {
+            this.propertyPreview = propertyPreview;
+            this.progressText = progressText;
+            images = new Integer[] {
+                    R.drawable.modern_apartment,
+                    R.drawable.modern_apartment_2
+            };
+            imageIndex = 0;
+            showNextProperty();
         }
 
-        propertyPreview.setImageResource(images[currentImage]);
+        // Creates a string that displays how the current image number
+        // out of the total images e.g. 10/31.
+        void updateProgressText() {
+            progressText.setText("10/31");
+        }
+
+        public void showNextProperty() {
+            ImageView propertyPreview = (ImageView) findViewById(R.id.propertyPreview);
+
+            imageIndex++;
+            if (imageIndex == images.length) {
+                imageIndex = 0;
+            }
+
+            propertyPreview.setImageResource(images[imageIndex]);
+            updateProgressText();
+        }
+
     }
 
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
